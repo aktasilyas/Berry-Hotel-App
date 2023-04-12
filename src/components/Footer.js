79b303f8d8c38c7d0297118d758f45f522a9cfa3
İlyas Aktaş <77029598+aktasilyas@users.noreply.github.com>
2023-04-13 00:16:13 +0300

    header ve css lerde düzenlemeler yapıldı

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Card } from "reactstrap";
+import "./style.css";
 const Footer = () => {
   return (
-    <div className="w-100 d-flex justify-content-center align-items-center bg-secondary p-5 text-white">
+    <div className="w-100 footer d-flex justify-content-center align-items-center p-5">
       <h3>Footer </h3>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row } from "reactstrap";
 import * as Icon from "react-feather";
+import "./style.css";
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 20) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="w-100 bg-secondary p-4 text-white d-flex justify-content-between align-items-center">
+    <div
+      className={`${
+        scrolled && "header-show"
+      } w-100 header  p-4  d-flex justify-content-between align-items-center`}
+    >
       <div className="d-flex">
         <Icon.Award size={25} />
         <h4 className="text-center">Berry Hotel</h4>
       </div>
       <div className="d-flex">
         <Icon.User size={25} />
-        <strong>Beyza GÜROCAK</strong>
+        <strong>İlyas AKTAŞ</strong>
       </div>
     </div>
   );
